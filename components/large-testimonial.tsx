bc30520287baa05295a2522dcbbe0b4b5180adfb
w1ld-resonation <no_nombre_otro@icloud.com>
2025-07-27 06:03:09 +0400

    Update large-testimonial.tsx

--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -1,43 +1,16 @@
-import Image from "next/image";
-import TestimonialImg from "@/public/images/large-testimonial.jpg";
-
 export default function LargeTestimonial() {
   return (
     <section>
       <div className="mx-auto max-w-2xl px-4 sm:px-6">
         <div className="py-12 md:py-20">
           <div className="space-y-3 text-center">
-            <div className="relative inline-flex">
-              <svg
-                className="absolute -left-6 -top-2 -z-10"
-                width={40}
-                height={49}
-                viewBox="0 0 40 49"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M22.7976 -0.000136375L39.9352 23.4746L33.4178 31.7234L13.7686 11.4275L22.7976 -0.000136375ZM9.34947 17.0206L26.4871 40.4953L19.9697 48.7441L0.320491 28.4482L9.34947 17.0206Z"
-                  fill="#D1D5DB"
-                />
-              </svg>
-              <Image
-                className="rounded-full"
-                src={TestimonialImg}
-                width={48}
-                height={48}
-                alt="WILDFLOW Testimonial"
-              />
-            </div>
             <p className="text-2xl font-bold text-gray-900">
-              “WILDFLOW unlocked new clarity in how we build, deploy and scale.
-              It’s not just infrastructure — it’s a philosophy of simplicity
-              and flow.”
+              “WILDFLOW is not just a platform — it's a new way of thinking. We simplify, we accelerate, and we empower teams around the world to build without friction.”
             </p>
             <div className="text-sm font-medium text-gray-500">
-              <span className="text-gray-700">Noah Levesque</span>{" "}
+              <span className="text-gray-700">Selim Ataniiazov</span>{" "}
               <span className="text-gray-400">/</span>{" "}
-              <span className="text-blue-500">Product Systems Architect</span>
+              <span className="text-blue-500">CEO at WILDFLOW</span>
             </div>
           </div>
         </div>
