6ca3249c02723883368b4ee07f1dac062cc1645b
w1ld-resonation <no_nombre_otro@icloud.com>
2025-07-27 05:59:38 +0400

    Update large-testimonial.tsx

--- components/large-testimonial.tsx
+++ components/large-testimonial.tsx
@@ -26,21 +26,18 @@ export default function LargeTestimonial() {
                 src={TestimonialImg}
                 width={48}
                 height={48}
-                alt="Large testimonial"
+                alt="WILDFLOW Testimonial"
               />
             </div>
             <p className="text-2xl font-bold text-gray-900">
-              “Simple has simplified my life in more ways than one. From
-              managing my sites to{" "}
-              <em className="italic text-gray-500">keeping track of tasks</em>,
-              it's become my go-to tool for everything.”
+              “WILDFLOW unlocked new clarity in how we build, deploy and scale.
+              It’s not just infrastructure — it’s a philosophy of simplicity
+              and flow.”
             </p>
             <div className="text-sm font-medium text-gray-500">
-              <span className="text-gray-700">Mary Sullivan</span>{" "}
+              <span className="text-gray-700">Noah Levesque</span>{" "}
               <span className="text-gray-400">/</span>{" "}
-              <a className="text-blue-500" href="#0">
-                CTO at Microsoft
-              </a>
+              <span className="text-blue-500">Product Systems Architect</span>
             </div>
           </div>
         </div>
