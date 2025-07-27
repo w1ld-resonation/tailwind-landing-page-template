964af6b38fb8647ed0798d52c3fe631f6f95e93d
w1ld-resonation <no_nombre_otro@icloud.com>
2025-07-27 07:43:42 +0400

    Update page.tsx

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
+  title: "WILDFLOW",
+  description: "DEV",
 };
 
 import Hero from "@/components/hero-home";
